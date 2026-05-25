022c84de81a5b90d3dc1ad61f038f121ca238ae1
abcangda <abcangda@gmail.com>
2026-05-24 23:22:10 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,25 @@
 const {
   Client,
   GatewayIntentBits,
-  SlashCommandBuilder,
   REST,
   Routes,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
   EmbedBuilder
 } = require("discord.js");
 
 const express = require("express");
-const Database = require("better-sqlite3");
-
-const echo = require("./bot2");
-const access = require("./bot3");
-const ban = require("./bot4");
 
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
-const db = new Database("db.db");
-
-db.exec(`
-CREATE TABLE IF NOT EXISTS messages (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  userId TEXT,
-  content TEXT,
-  createdAt INTEGER
-);
-`);
-
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
   ]
 });
 
@@ -45,29 +31,11 @@ app.get("/", (req,res)=>{
 
 app.listen(3000);
 
-function topKey(obj) {
-
-  return Object.keys(obj)
-    .sort((a,b)=>obj[b]-obj[a])[0];
-}
-
 const commands = [
 
-  echo.echoCommand,
-  echo.addEchoCommand,
-  echo.removeEchoCommand,
-  echo.echoListCommand,
-
-  access.command,
-
-  ban.command,
-
   new SlashCommandBuilder()
-
     .setName("scan")
-
     .setDescription("Escanea un usuario")
-
     .addUserOption(option =>
       option
         .setName("usuario")
@@ -76,11 +44,8 @@ const commands = [
     ),
 
   new SlashCommandBuilder()
-
     .setName("file")
-
     .setDescription("Expediente completo")
-
     .addUserOption(option =>
       option
         .setName("usuario")
@@ -89,71 +54,81 @@ const commands = [
     ),
 
   new SlashCommandBuilder()
-
     .setName("intel")
-
-    .setDescription("Analiza similitud")
-
+    .setDescription("Compara usuarios")
     .addUserOption(option =>
       option
         .setName("u1")
         .setDescription("Usuario 1")
         .setRequired(true)
     )
-
     .addUserOption(option =>
       option
         .setName("u2")
         .setDescription("Usuario 2")
         .setRequired(true)
+    ),
+
+  new SlashCommandBuilder()
+    .setName("echo")
+    .setDescription("Hace hablar al bot")
+    .addStringOption(option =>
+      option
+        .setName("texto")
+        .setDescription("Texto")
+        .setRequired(true)
+    ),
+
+  new SlashCommandBuilder()
+    .setName("ban")
+    .setDescription("Banear usuario")
+    .addUserOption(option =>
+      option
+        .setName("usuario")
+        .setDescription("Usuario")
+        .setRequired(true)
     )
 
-].map(x=>x.toJSON());
+].map(command => command.toJSON());
 
 async function registerCommands() {
 
   const rest = new REST({
-    version:"10"
+    version: "10"
   }).setToken(TOKEN);
 
-  await rest.put(
-    Routes.applicationCommands(CLIENT_ID),
-    { body: [] }
-  );
+  try {
 
-  await rest.put(
-    Routes.applicationGuildCommands(
-      CLIENT_ID,
-      GUILD_ID
-    ),
-    { body: commands }
-  );
+    console.log("Limpiando comandos...");
+
+    await rest.put(
+      Routes.applicationCommands(CLIENT_ID),
+      { body: [] }
+    );
+
+    console.log("Registrando comandos...");
+
+    await rest.put(
+      Routes.applicationGuildCommands(
+        CLIENT_ID,
+        GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log("Comandos registrados.");
+
+  } catch(err) {
+
+    console.error(err);
+  }
 }
 
-client.once("clientReady", async ()=>{
+client.once("ready", async ()=>{
 
-  console.log(`ONLINE ${client.user.tag}`);
+  console.log(`${client.user.tag} online`);
 
   await registerCommands();
-});
-
-client.on("messageCreate", message=>{
-
-  if (message.author.bot)
-    return;
-
-  db.prepare(`
-    INSERT INTO messages (
-      userId,
-      content,
-      createdAt
-    )
-    VALUES (?, ?, ?)
-  `).run(
-    message.author.id,
-    message.content,
-    Date.now()
-  );
 });
 
 client.on("interactionCreate", async interaction=>{
@@ -163,151 +138,10 @@ client.on("interactionCreate", async interaction=>{
 
   try {
 
-    if (
-      interaction.commandName === "echo"
-      ||
-      interaction.commandName === "addecho"
-      ||
-      interaction.commandName === "removeecho"
-      ||
-      interaction.commandName === "echolist"
-    ) {
-      return echo.execute(interaction);
-    }
-
-    if (
-      interaction.commandName === "access"
-    ) {
-      return access.execute(interaction);
-    }
-
-    if (
-      interaction.commandName === "ban"
-    ) {
-      return ban.execute(interaction);
-    }
-
-    if (
-      interaction.commandName === "scan"
-    ) {
+    if (interaction.commandName === "scan") {
 
       const user =
         interaction.options.getUser("usuario");
-
-      const messages = db.prepare(`
-        SELECT * FROM messages
-        WHERE userId = ?
-      `).all(user.id);
-
-      let score = 0;
-
-      const age =
-        (Date.now() - user.createdTimestamp)
-        / 86400000;
-
-      if (age < 7)
-        score += 50;
-
-      if (messages.length < 20)
-        score += 20;
-
-      const risk =
-        score >= 70
-        ? "HIGH"
-        : score >= 40
-        ? "MEDIUM"
-        : "LOW";
-
-      return interaction.reply({
-
-        embeds: [
-
-          new EmbedBuilder()
-
-            .setColor(0x0A0A0A)
-
-            .setTitle("VOUGHT THREAT ANALYSIS")
-
-            .setDescription(
-              "Análisis completado."
-            )
-
-            .setThumbnail(
-              user.displayAvatarURL()
-            )
-
-            .addFields(
-
-              {
-                name:"Usuario",
-                value:user.tag,
-                inline:true
-              },
-
-              {
-                name:"Threat Score",
-                value:`${score}/100`,
-                inline:true
-              },
-
-              {
-                name:"Threat Level",
-                value:risk,
-                inline:true
-              }
-
-            )
-
-            .setImage(
-              score >= 70
-              ? "https://cdn.discordapp.com/attachments/1494932411702710312/1507467664933519440/image0.gif?ex=6a12022a&is=6a10b0aa&hm=9338b5fbf6feaa55d7b580f890c4a81f185ff916efbeb949045e996042d06577&"
-              : "https://cdn.discordapp.com/attachments/1494932411702710312/1507487015065878558/image0.gif?ex=6a121430&is=6a10c2b0&hm=8d4aedc1166df6c662117fb772c14fefc161d85ba1f8de039b399df660b865c7&"
-            )
-
-        ]
-      });
-    }
-
-    if (
-      interaction.commandName === "file"
-    ) {
-
-      const user =
-        interaction.options.getUser("usuario");
-
-      const member =
-        await interaction.guild.members.fetch(
-          user.id
-        );
-
-      const messages = db.prepare(`
-        SELECT * FROM messages
-        WHERE userId = ?
-      `).all(user.id);
-
-      let words = {};
-      let hours = {};
-
-      for (const msg of messages) {
-
-        for (const word of msg.content
-          .toLowerCase()
-          .split(/\s+/)) {
-
-          if (word.length < 4)
-            continue;
-
-          words[word] =
-            (words[word] || 0) + 1;
-        }
-
-        const h =
-          new Date(msg.createdAt)
-          .getHours();
-
-        hours[h] =
-          (hours[h] || 0) + 1;
-      }
 
       return interaction.reply({
 
@@ -317,7 +151,13 @@ client.on("interactionCreate", async interaction=>{
 
             .setColor(0x0A0A0A)
 
-            .setTitle("VOUGHT USER FILE")
+            .setTitle(
+              "VOUGHT THREAT ANALYSIS"
+            )
+
+            .setDescription(
+              `${user.tag} fue analizado.`
+            )
 
             .setThumbnail(
               user.displayAvatarURL()
@@ -326,32 +166,14 @@ client.on("interactionCreate", async interaction=>{
             .addFields(
 
               {
-                name:"Usuario",
-                value:user.tag,
+                name:"Threat Level",
+                value:"LOW",
                 inline:true
               },
 
               {
-                name:"Display",
-                value:member.displayName,
-                inline:true
-              },
-
-              {
-                name:"Mensajes",
-                value:`${messages.length}`,
-                inline:true
-              },
-
-              {
-                name:"Palabra favorita",
-                value:topKey(words) || "Ninguna",
-                inline:true
-              },
-
-              {
-                name:"Hora activa",
-                value:`${topKey(hours)||0}:00`,
+                name:"Classification",
+                value:"Civilian",
                 inline:true
               }
 
@@ -362,12 +184,59 @@ client.on("interactionCreate", async interaction=>{
             )
 
         ]
+
       });
     }
 
-    if (
-      interaction.commandName === "intel"
-    ) {
+    if (interaction.commandName === "file") {
+
+      const user =
+        interaction.options.getUser("usuario");
+
+      return interaction.reply({
+
+        embeds:[
+
+          new EmbedBuilder()
+
+            .setColor(0x0A0A0A)
+
+            .setTitle(
+              "VOUGHT USER FILE"
+            )
+
+            .setDescription(
+              `Expediente de ${user.tag}`
+            )
+
+            .setThumbnail(
+              user.displayAvatarURL()
+            )
+
+            .addFields(
+
+              {
+                name:"Cuenta creada",
+                value:`<t:${parseInt(user.createdTimestamp / 1000)}:R>`
+              },
+
+              {
+                name:"Usuario ID",
+                value:user.id
+              }
+
+            )
+
+            .setImage(
+              "https://cdn.discordapp.com/attachments/1494932411702710312/1507486803471503502/image0.gif?ex=6a1213fd&is=6a10c27d&hm=ed8d86f49456bd75709c899f720387e7038d0fb879cb859647bbeaab92790aa6&"
+            )
+
+        ]
+
+      });
+    }
+
+    if (interaction.commandName === "intel") {
 
       const u1 =
         interaction.options.getUser("u1");
@@ -375,26 +244,21 @@ client.on("interactionCreate", async interaction=>{
       const u2 =
         interaction.options.getUser("u2");
 
-      let similarity = 0;
+      let similarity = 20;
 
       if (
-        u1.username.slice(0,3)
-        .toLowerCase()
+        u1.username
+          .slice(0,3)
+          .toLowerCase()
 
         ===
 
-        u2.username.slice(0,3)
-        .toLowerCase()
+        u2.username
+          .slice(0,3)
+          .toLowerCase()
       ) {
-        similarity += 20;
+        similarity += 40;
       }
-
-      const level =
-        similarity >= 70
-        ? "HIGH"
-        : similarity >= 40
-        ? "MEDIUM"
-        : "LOW";
 
       return interaction.reply({
 
@@ -423,28 +287,115 @@ client.on("interactionCreate", async interaction=>{
               },
 
               {
-                name:"Compatibilidad",
-                value:`${similarity}% • ${level}`,
+                name:"Coincidencia",
+                value:`${similarity}%`,
                 inline:true
               }
 
             )
 
             .setImage(
-              similarity >= 70
-              ? "https://cdn.discordapp.com/attachments/1494932411702710312/1507467664933519440/image0.gif?ex=6a12022a&is=6a10b0aa&hm=9338b5fbf6feaa55d7b580f890c4a81f185ff916efbeb949045e996042d06577&"
-              : "https://cdn.discordapp.com/attachments/1494932411702710312/1507486803471503502/image0.gif?ex=6a1213fd&is=6a10c27d&hm=ed8d86f49456bd75709c899f720387e7038d0fb879cb859647bbeaab92790aa6&"
+              similarity >= 60
+
+              ?
+
+              "https://cdn.discordapp.com/attachments/1494932411702710312/1507467664933519440/image0.gif?ex=6a12022a&is=6a10b0aa&hm=9338b5fbf6feaa55d7b580f890c4a81f185ff916efbeb949045e996042d06577&"
+
+              :
+
+              "https://cdn.discordapp.com/attachments/1494932411702710312/1507467086182613173/image0.gif?ex=6a1201a0&is=6a10b020&hm=8339f4069c66da70af9c9de5da031dc253c62225126292fd71ef278e16e30b46&"
             )
 
         ]
+
       });
     }
 
-  }
+    if (interaction.commandName === "echo") {
 
-  catch(err) {
+      const text =
+        interaction.options.getString(
+          "texto"
+        );
+
+      await interaction.reply({
+        content:"Transmitiendo...",
+        ephemeral:true
+      });
+
+      return interaction.channel.send(text);
+    }
+
+    if (interaction.commandName === "ban") {
+
+      if (
+        interaction.user.id !==
+        "1305030009681088592"
+      ) {
+
+        return interaction.reply({
+
+          content:
+            "Homelander rechazó tu solicitud.",
+
+          ephemeral:true
+        });
+      }
+
+      const user =
+        interaction.options.getUser(
+          "usuario"
+        );
+
+      const member =
+        await interaction.guild.members
+          .fetch(user.id);
+
+      await member.ban();
+
+      await interaction.reply({
+        content:"Objetivo eliminado.",
+        ephemeral:true
+      });
+
+      return interaction.channel.send({
+
+        embeds:[
+
+          new EmbedBuilder()
+
+            .setColor(0x0A0A0A)
+
+            .setTitle(
+              "TARGET ELIMINATED"
+            )
+
+            .setDescription(
+              `Homelander asesinó a ${user.tag}.`
+            )
+
+            .setImage(
+              "https://cdn.discordapp.com/attachments/1494932411702710312/1507467664933519440/image0.gif?ex=6a12022a&is=6a10b0aa&hm=9338b5fbf6feaa55d7b580f890c4a81f185ff916efbeb949045e996042d06577&"
+            )
+
+        ]
+
+      });
+    }
+
+  } catch(err) {
 
     console.error(err);
+
+    if (!interaction.replied) {
+
+      interaction.reply({
+
+        content:"Error interno.",
+
+        ephemeral:true
+      });
+    }
   }
 });
 
